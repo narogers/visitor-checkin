edd533b47d3b630d1bc772c93eab4d88d017885b
Nathan Rogers <nrogers@clevelandart.org>
2015-02-11 15:07:01 -0500

    Preparing a clean slate to begin the transition to CakePHP.
    All the original source files have been taken out in this commit so that an initial version based around CakePHP can be dropped in even though the model itself is still in some flux. Using an MVC framework should speed development as well as make it easier to maintain in the future.

--- partials/header.php
+++
@@ -1,5 +0,0 @@
-<div data-role="header" data-theme="b" data-position="fixed">
-    <a data-icon="home" href="/checkin" onclick="fullReset();" data-transition="slideright" data-theme="b">RESET</a>
-    <!--<a data-icon="check" href="#checkIn" data-theme="b">Check-In</a>-->
-    <h1>Ingalls Library Patron Check-in Kiosk</h1>
-</div>
