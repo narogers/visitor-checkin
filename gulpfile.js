599f497651b26c1c41873e162db092d8eaec7226
Nathan Rogers <nrogers@clevelandart.org>
2016-05-19 14:52:29 -0400

    [WIP] Use gulp to manage Bootstrap and jQuery

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,16 @@ var elixir = require('laravel-elixir');
  | file for our application, as well as publishing vendor resources.
  |
  */
+var paths = {
+  'bootstrap': './resources/assets/vendor/bootstrap-sass/assets/',
+  'jquery': './resources/assets/vendor/jquery/'
+}
 
 elixir(function(mix) {
-    mix.less('app.less');
+    mix.sass('app.scss', 'public/css/', {includePaths: [paths.bootstrap + 'stylesheets/']})
+      .copy(paths.bootstrap + 'fonts/bootstrap/**', 'public/fonts')
+      .scripts([
+        paths.jquery + "dist/jquery.js",
+        paths.bootstrap + "javascripts/bootstrap.js"
+      ], "./", "public/js/app.js");
 });
