eb07c47cc2098d0b512494bbe5cdc89f17c898a4
Nathan Rogers <nrogers@clevelandart.org>
2016-05-19 15:47:29 -0400

    [WIP] Fix migration problems to Laravel 5.2

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,11 +1,9 @@
 <?php namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Bus\DispatchesCommands;
+use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
-abstract class Controller extends BaseController {
-
-	use DispatchesCommands, ValidatesRequests;
-
+class Controller extends BaseController {
+	use DispatchesJobs, ValidatesRequests;
 }
