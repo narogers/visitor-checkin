d838ba23732f97a163046dede02362b53cdf03e2
Nathan Rogers <nrogers@clevelandart.org>
2016-06-02 16:41:07 -0400

    [WIP] Properly implement interface with Aleph

--- app/ILS/AlephService.php
+++ app/ILS/AlephService.php
@@ -19,18 +19,47 @@ class AlephService implements ILSInterface {
     $this->alephXService = "http://" . Config('ils.host', 'localhost') .
       "/X?";
     Log::info("[ILSClient] Aleph connection initialized and ready for use");
-    Log::info("[ILSClient] Using " . $this->alephWebService . " for requests");  
+    Log::info("[ILSClient] Using URI base " . $this->alephWebService . " for requests");  
   }
 
   /**
-   * Calculate the default keys for a patron based on specific logic. This
-   * can be swapped out for different use cases easily.
+   * Uses the name to algorithmicly create possible
+   * matches based on the standard Ingalls library 
+   * practice of <first initial>.<last name> truncated
+   * to a total length of 12 characters
+   *
+   * ie
+   * James Joyce -> j.joyce
+   * Leopold Bloom -> l.bloom
+   * HC Earwigger -> h.earwigger
    *
    * @param name
    * @return array
    */
-  public function getIdentifiers($name) {
-    return [];
+  public function getIdentifiers($input) {
+  	$pieces = [];
+  	preg_match("/^(\w+)\s?.*\s([-a-zA-Z]+)$/", $input, $pieces);
+
+  	$name['initial'] = '';
+  	$name['last'] = '';
+
+  	if (preg_match("/,/", $input)) {
+  		$name['initial'] = $pieces[2];
+  		$name['last'] = $pieces[1];
+  	} else {
+  		$name['initial'] = $pieces[1];
+  		$name['last'] = $pieces[2];
+  	}
+  	$name['initial'] = strtoupper(substr($name['initial'], 0, 1));
+  	$name['last'] = preg_replace("/\W+/", "", $name['last']);
+  	$name['last'] = strtoupper(substr($name['last'], 0, 10));
+  	
+  	$identifiers = [
+      $name['initial'] . "." . $name['last'],
+  	  $name['initial'] . $name['last']
+    ];
+
+    return $identifiers;
   }
 
   /**
@@ -40,7 +69,23 @@ class AlephService implements ILSInterface {
    * @return string
    */
   public function getILSId($identifier) {
-    return "foo.bar";
+    $response = file_get_contents($this->endpointFor('id', $identifier));
+	$aleph_data = simplexml_load_string($response);
+
+	/*
+	 * The two most likely responses are
+	 *
+	 * <error>Error in verification</error>
+	 * <internal-id>[Internal ID for patron]</internal-id>
+	 */
+	if ($aleph_data->{'error'}) {
+		Log::info("[ALEPH] Could not resolve " . $identifier . " to a canonical ID");
+		Log::info("[ALEPH] ${response}");
+        return null;
+	} 
+	if ($aleph_data->{'internal-id'}) {
+		return $aleph_data->{'internal-id'}->__toString();
+	}
   }
 
   /**
@@ -51,7 +96,21 @@ class AlephService implements ILSInterface {
    * @return boolean
    */
   public function isActive($identifier) {
-    return true;
+    if (null == $identifier) {
+      return false;
+    }
+
+    $response = file_get_contents($this->endpointFor("status", $identifier));
+    $aleph_data = simplexml_load_string($response);
+    $expires_on = $aleph_data->xpath("//z305-expiry-date");
+    /**
+     * If we get an error instead of the record then we'll just set the date
+     * to null. This can be done easily by popping off the first value even if
+     * the value is []
+     */
+    $expires_on = array_pop($expires_on);
+ 
+    return (strtotime($expires_on) > time());
   }
 
   /**
@@ -72,10 +131,75 @@ class AlephService implements ILSInterface {
    * @return array
    */
   public function getPatronDetails($identifier) {
-    return [
-      "name" => "Uncle Wiggly",
-      "email" => "wiggly@rabbits.org",
-      "role" => "Member"
-    ];
-  }  
+    $response = file_get_contents($this->endpointFor("address", $identifier));
+    $patron_details = simplexml_load_string($response);
+
+    $response = file_get_contents($this->endpointFor("status", $identifier));
+    $patron_status = simplexml_load_string($response);
+
+    /**
+     * 0002 is Aleph speak for an invalid patron request
+     */
+    $reply_code = $patron_details->xpath("//reply-code")[0];
+    if ("0002" == $reply_code) {
+      return null;
+    }
+
+    /**
+     * Otherwise press on and extract the relevant details from both feeds
+     */
+    $details = [];
+    $details["name"] = $this->getFirstValueFor($patron_details, "//z304-address-1");
+    $details["email"] = trim($this->getFirstValueFor($patron_details, "//z304-email-address"));
+    $details["role"] = $this->getFirstValueFor($patron_status, "//z305-bor-type");
+    $details["id"] = $identifier;
+
+    return $details;
+  } 
+
+  /**
+   * Retrieve the canonical endpoint for a particular service. A helper 
+   * method which should not be publicly exposed
+   *
+   * @param string $endpoint
+   * @param string $identifier
+   * @return string $path
+   */
+  protected function endpointFor($endpoint, $identifier) {
+    $uri = null;
+
+    switch ($endpoint) {
+	  case 'address':
+		$uri = $this->alephWebService . "/patron/" . urlencode($identifier) . "/patronInformation/address";
+		break;
+	  case 'id':
+		$uri = $this->alephXService . "library=CMA50&op=bor_by_key&bor_id=" . urlencode($identifier);
+        break;
+	  case 'status':
+	    $uri = $this->alephWebService . "/patron/" . urlencode($identifier) . "/patronStatus/registration";
+		break;
+	  default:
+	    Log::warning('ERROR: Could not resolve endpoint for ${endpoint}');
+	  }
+    Log::info("[ILSClient] Initializing call to ${uri}");
+ 
+    return $uri;
+  }
+
+  /**
+   * Gets the value from an XPath expression and returns it as a string.
+   * If no matches it will return null
+   *
+   * @param $xml
+   * @param string $xpath
+   * @return string
+   */
+  protected function getFirstValueFor($xml, $xpath) {
+    $values = $xml->xpath($xpath);
+    if (0 == count($values)) {
+      return null;
+    } else {
+      return $values[0]->__toString();
+    }
+  }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -1,13 +1,11 @@
 <?php namespace App\Models;
 
-use Carbon\Carbon;
-
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Log;
 
-use App\Checkin;
-use App\Registration;
-use App\Role;
+use App\Models\Checkin;
+use App\Models\Registration;
+use App\Models\Role;
 
 class User extends Model {
 	/**
@@ -17,31 +15,16 @@ class User extends Model {
 	 */
 	protected $fillable = ['name', 'email_address'];
 
-    /**
-     * Masks the email address for privacy when listing multiple values
-     *
-     * eg foo@bar.com would become f*****@cl*****.org
-     */
-   public function masked_email() {
-     list($account, $domain) = explode("@", $this->email_address);
-     $account_mask = substr($account, 0, 2) . str_repeat("*", 5);
-     $domain_components = explode(".", $domain);
-     $domain_mask = substr($domain_components[0], 0, 2) . str_repeat("*", 5);
-     $tld = array_pop($domain_components);
-
-     return "${account_mask}@${domain_mask}.${tld}";
-   }
-
 	public function role() {
-		return $this->belongsTo('App\Role');
+		return $this->belongsTo('App\Models\Role');
 	}
 
 	public function registration() {
-		return $this->hasOne('App\Registration');
+		return $this->hasOne('App\Models\Registration');
 	}
 
 	public function checkins() {
-		return $this->hasMany('App\Checkin');
+		return $this->hasMany('App\Models\Checkin');
 	}
 
     /**
@@ -63,134 +46,6 @@ class User extends Model {
     }
 
 	/**
-	 * Construct a list of potential keys to use when
-	 * resolving calls to Aleph. If the ID and barcode are
-	 * set they will be used. Otherwise it will try to
-	 * generate options based on the name
-	 */
-	public function getAlephKeys() {
-		$keys = [];
-		if (!empty($this->aleph_id)) {
-			array_push($keys, $this->aleph_id);
-		}
-		if (!empty($this->barcode)) {
-			array_push($keys, $this->barcode);
-		}
-
-		# If it is still empty then generate keys
-		if (empty($keys)) {
-			$this->generateAlephKeys($keys);
-		}
-		return $keys;
-	}
-
-	/**
-	 * Return the primary Aleph key as calculated by
-	 * the other helper method(s)
-	 */
-  public function getAlephKey() {
-  	$aleph_keys = $this->getAlephKeys();
-  	return array_shift($aleph_keys);
-  }
-
-  /**
-   * Uses the name to algorithmicly create possible
-   * matches based on the standard Ingalls library 
-   * practice of <first initial>.<last name> truncated
-   * to a total length of 12 characters
-   *
-   * ie
-   * James Joyce -> j.joyce
-   * Leopold Bloom -> l.bloom
-   * HC Earwigger -> h.earwigger
-   */
-  public function generateAlephKeys(&$keys) {
-  	# First normalize the names to accept only the first
-  	# and last strings. If a comma is present then invert
-  	# them
-  	$pieces = [];
-  	preg_match("/^(\w+)\s?.*\s([-a-zA-Z]+)$/", $this->name, 
-  		$pieces);
-
-  	$name['initial'] = '';
-  	$name['last'] = '';
-
-  	if (preg_match("/,/", $this->name)) {
-  		$name['initial'] = $pieces[2];
-  		$name['last'] = $pieces[1];
-  	} else {
-  		$name['initial'] = $pieces[1];
-  		$name['last'] = $pieces[2];
-  	}
-  	$name['initial'] = strtoupper(substr($name['initial'], 
-  		0, 1));
-  	$name['last'] = preg_replace("/\W+/", "", $name['last']);
-  	$name['last'] = strtoupper(substr($name['last'], 
-  		0, 10));
-  	
-  	$keys[0] = $name['initial'] . "." . $name['last'];
-  	$keys[1] = $name['initial'] . $name['last'];
-  }
-
-	/**
-	 * Inserts a checkin
-	 *
-	 * TODO: Prevent duplicate entries for the same day by updating a record rather than just carte blanche
-	 *       inserting a new one
-	 */
-	public function addCheckin() {
-		# Check for existing checkins before you insert a new one. Otherwise just
-		# scroll right past
-		$not_checked_in = (0 == $this->checkins()
-			->where('created_at', '>=', Carbon::now())->count());
-		if ($not_checked_in) {
-			$checkin = new Checkin();
-  		$this->checkins()->save($checkin);
-		}
-	}
-
-	public function formattedCreationDate() {
-		return Carbon::parse($this->created_at)->toFormattedDateString();
-	}
-
-	/**
-	 * For this first pass it does not filter out any dates that may be
-	 * beyond the range provided. When supporting the view for the 
-	 * previous month this may be a consideration
-	 */
-	public function formattedLastCheckin($starting = null, 
-		$ending = null) {
-		$starting = empty($starting) ? Carbon::now() : $starting;
-		
-		$last_checkin = $this->checkins()->during($starting, $ending)->orderBy('created_at', 'DESC')->first();
-		if (null == $last_checkin) {
-			return "Not available";
-		} else {
-			return Carbon::parse($last_checkin->created_at)->format('F jS');
-		}
-	}
-
-	/**
-	 * Retrieve a list of checkins from a specific
-	 * range. If passed 'daily', 'weekly', 'monthly',
-	 * or 'lastmonth' then convert to natural dates.
-	 * Otherwise use the dates as provided to scope
-	 * the query
-	 */
-	public function checkinsDuring($starting, $ending = null) {
-		return $this->checkins()->during($starting,
-			$ending)->get();
-	}
-
-	/**
-	 * Utility method that returns only the count of checkins
-	 * over a specific range rather than the actual values
-	 */
-	public function checkinCountFor($range) {
-		return $this->checkinsDuring($range)->count();
-	}
-
-	/**
 	 * Generates a fake email address by hashing the date and time. This is only
 	 * a placeholder for edge cases where the email field is empty and not meant to
 	 * be used routinely
@@ -198,40 +53,9 @@ class User extends Model {
 	 * @return String
 	 */
 	protected function generateEmailStub() {
-		$host = "null.null";
+		$host = "cma.local";
 		$address = substr(hash('md5', date('Ymd His' . microtime(true))), 0, 16);
 
-		$faux_email = $address . "@" . $host;
-		return $faux_email;
-	}
-
-	/**
-	 * Given a string tries to convert it to a pair of dates. If
-	 * the end date is null or the first date contains a human
-	 * readable string it will be cast to today's date instead
-	 * of parsed
-	 */
-	protected function normalizeDate(&$range) {
-		switch($range[0]) {
-			case "daily":
-				$range[0] = Carbon::now();
-				$range[1] = Carbon::now();
-				break;
-			case "weekly":
-				$range[0] = Carbon::now()->startOfWeek();
-				$range[1] = Carbon::now();
-				break;
-			case "monthly":
-				$range[0] = Carbon::now()->startOfMonth();
-				$range[1] = Carbon::now();
-				break;
-			case "lastmonth":
-				$range[0] = Carbon::now()->startOfMonth()->subMonth(1);
-				$range[1] = Carbon::now()->subMonth(1)->endOfMonth();
-				break;
-      default:
-        $range[0] = Carbon::parse($range[0]);
-        $range[1] = empty($range[1]) ? Carbon::now() : Carbon::parse($range[1]);
-		}
+		return "${address}@${host}";
 	}
 }
